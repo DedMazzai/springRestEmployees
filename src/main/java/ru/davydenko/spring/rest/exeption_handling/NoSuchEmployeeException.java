a83a7d5e5eaf7b8b684fc6b8419eff4b771c7e43
DedMazzai <ded_mazzai@inbox.ru>
2023-07-30 19:54:04 +0300

    add exceptions

---
+++ src/main/java/ru/davydenko/spring/rest/exeption_handling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package ru.davydenko.spring.rest.exeption_handling;
+
+public class NoSuchEmployeeException extends RuntimeException{
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
